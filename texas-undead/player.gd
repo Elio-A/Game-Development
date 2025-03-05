5d0280af2232e68b6bcfb044536cf037955444d1
Khaled <k9developments@outlook.com>
2025-03-04 23:50:31 -0800

    Updated Player
    - Added movement script (Forward, backward, left, right, and jump)
    - Added collision (Still not working, player falls through the ground)

--- texas-undead/player.gd
+++ texas-undead/player.gd
@@ -1,5 +1,8 @@
-extends Area3D
+extends CharacterBody3D
 
+@export var speed = 10
+@export var gravity = -9
+@export var jumpForce = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,6 +10,25 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("Forward"):
+func _physics_process(delta: float) -> void:
+	#Getting input direction:
+	var inputDirection = Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction = (transform.basis * Vector3(inputDirection.x, 0, inputDirection.y)).normalized()
+	
+	#Applying Gravity:
+	if not is_on_floor():
+		velocity.y += gravity * delta
 		
+	#Jump:
+	if Input.is_action_pressed("Jump") and is_on_floor():
+		velocity.y = jumpForce
+		
+	#Set Horizontal Velocity:
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+	move_and_slide()
