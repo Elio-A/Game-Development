63aaa7ad6deddd8a6bf11a1723de733a22975a38
asaba117 <asaba1@unb.ca>
2025-03-20 22:17:37 -0300

    Menu and menu resources done.
    Menu done, but some minor visual improvemnts may be done in future versions. also added game logo

---
+++ texas-undead/Menus/main_menu.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://ground.tscn")
+
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/OptionsMenu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit(1)
